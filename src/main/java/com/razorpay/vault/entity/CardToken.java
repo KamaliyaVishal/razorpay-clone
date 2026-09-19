9aa6041898ce5d55a6b39f1a07ab0902e6fdb439
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-19 22:18:15 +0530

    CardToken entity

---
+++ src/main/java/com/razorpay/vault/entity/CardToken.java
@@ -0,0 +1,39 @@
+package com.razorpay.vault.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "card_token")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @Column(name = "merchant_id", nullable = false)
+    private UUID merchantId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "vault_card_id", nullable = false)
+    private VaultCard vaultCard;
+
+    @Column(name = "token", nullable = false, length = 50, unique = true)
+    private String token;
+
+    @Column(name = "revoked_at")
+    private LocalDateTime revokedAt;
+
+}
