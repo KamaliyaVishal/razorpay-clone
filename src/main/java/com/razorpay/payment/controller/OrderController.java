a06ec4e0be6a32518386c216fa515a2715f078b7
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-21 18:01:39 +0530

    Update OrderController

--- src/main/java/com/razorpay/payment/controller/OrderController.java
+++ src/main/java/com/razorpay/payment/controller/OrderController.java
@@ -2,16 +2,15 @@ package com.razorpay.payment.controller;
 
 import com.razorpay.payment.dto.request.CreateOrderRequest;
 import com.razorpay.payment.dto.response.OrderResponse;
+import com.razorpay.payment.dto.response.PaymentResponse;
 import com.razorpay.payment.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,10 +21,29 @@ public class OrderController {
     private final OrderService orderService;
     UUID merchantId = UUID.fromString("73936a07-f285-4930-9dab-1801ede02d8c");
 
+
     @PostMapping
     public ResponseEntity<OrderResponse> create(@RequestBody @Valid CreateOrderRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(orderService.createOrder(merchantId, request));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> fetchOrder(@PathVariable UUID orderId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(orderService.getOrderById(merchantId, orderId));
+    }
+
+    @PostMapping("/{orderId}/cancel")
+    public ResponseEntity<OrderResponse> cancelOrder(@PathVariable UUID orderId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(orderService.cancelOrder(merchantId, orderId));
+    }
+
+    @GetMapping("/payments")
+    public ResponseEntity<List<PaymentResponse>> fetchAllPayments(@PathVariable UUID orderId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(orderService.listPayments(merchantId, orderId));
     }
 
 }
