95a9eb2aececc0df2bcbd3743c16a6a7b2aa38a9
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-23 00:31:52 +0530

    Setup Bank Callback Simulator

---
+++ src/main/java/com/razorpay/common/enums/ChaosMode.java
@@ -0,0 +1,9 @@
+package com.razorpay.common.enums;
+
+public enum ChaosMode {
+    NORMAL,
+    SLOW,
+    FAILURE,
+    SUCCESS,
+    TIMEOUT
+}
--- src/main/java/com/razorpay/payment/repository/PaymentRepository.java
+++ src/main/java/com/razorpay/payment/repository/PaymentRepository.java
@@ -1,9 +1,11 @@
 package com.razorpay.payment.repository;
 
+import com.razorpay.common.enums.PaymentStatus;
 import com.razorpay.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,4 +16,6 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     List<Payment> findAllByOrderRecord_Id(UUID orderId);
 
     Optional<Payment> findByMerchantIdAndId(UUID merchantId, UUID paymentId);
+
+    List<Payment> findByStatusAndCreatedAtBefore(PaymentStatus paymentStatus, LocalDateTime globalWindow);
 }
