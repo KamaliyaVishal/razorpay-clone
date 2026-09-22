7a3d5f791fc7589dba8b274d411a31f96b936180
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-22 17:37:29 +0530

    Implement Payment Transition Service

---
+++ src/main/java/com/razorpay/payment/statemachine/PaymentTransitionService.java
@@ -0,0 +1,38 @@
+package com.razorpay.payment.statemachine;
+
+import com.razorpay.common.enums.PaymentActor;
+import com.razorpay.common.enums.PaymentEvent;
+import com.razorpay.common.enums.PaymentStatus;
+import com.razorpay.payment.entity.Payment;
+import com.razorpay.payment.entity.PaymentTransitionLog;
+import com.razorpay.payment.repository.PaymentTransitionLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentTransitionService {
+
+    private final PaymentStateMachine paymentStateMachine;
+    private final PaymentTransitionLogRepository paymentTransitionLogRepository;
+
+    public PaymentStatus apply(Payment payment, PaymentEvent paymentEvent) {
+
+        PaymentStatus next = paymentStateMachine.transition(payment.getStatus(), paymentEvent);
+
+        PaymentTransitionLog paymentTransitionLog = PaymentTransitionLog.builder()
+                .payment(payment)
+                .fromStatus(payment.getStatus())
+                .paymentEvent(paymentEvent)
+                .toStatus(next)
+                .paymentActor(PaymentActor.SYSTEM)
+                .occurrenceAt(LocalDateTime.now())
+                .build();
+        paymentTransitionLogRepository.save(paymentTransitionLog);
+
+        return next;
+    }
+
+}
