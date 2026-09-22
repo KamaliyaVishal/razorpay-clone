2906c4dd4a9d90fb615f46f6b89dfbfbcb6762b1
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-22 16:25:28 +0530

    Implement mock NetBanking/UPI payment processor

--- src/main/java/com/razorpay/payment/service/Impl/PaymentServiceImpl.java
+++ src/main/java/com/razorpay/payment/service/Impl/PaymentServiceImpl.java
@@ -76,6 +76,9 @@ public class PaymentServiceImpl implements PaymentService {
                 payment.setErrorCode(failure.errorCode());
                 payment.setErrorDescription(failure.errorDescription());
             }
+            case PaymentResult.Success success ->{
+
+            }
         }
 
         payment = paymentRepository.save(payment);
